d463bdb2c3dae8ca1070e8a3e929c1a1a6ca318c
Rafael Guterres Jeffman <rjeffman@redhat.com>
2024-12-13 19:28:51 -0300

    Convert container names to FQDN
    The container names are defined as free strings where the only rules
    that apply (but not enforced) are Podman rules for container names.
    
    Usually the names for the containers contain a single word, as they
    represent services in a compose, but in the case of a FreeIPA cluster,
    it makes sense to have the container names to mimic the hostnames,
    specially in multi-deployment environments.
    
    This change add support for the global configuration 'container_fqdn'.
    When set to 'true', if  the container is a single word, the name is
    appended by the deployment domain, with the container name becoming
    "<name>.<domain>" in the same way that the hostnames are derived.
    
    By default, 'container_fqdn' is 'false'.
    
    With this change, when using 'container_fqdn: true' the podman network
    nameserver resolves the names correctly and replicas can be deployed
    even with multiple-deployment scenarios.

--- ipalab_config/compose.py
+++ ipalab_config/compose.py
@@ -9,7 +9,9 @@ from ipalab_config.utils import die, get_hostname, is_ip_address, ensure_fqdn
 IP_GENERATOR = iter(range(10, 255))
 
 
-def get_compose_config(containers, network, distro, ips=IP_GENERATOR):
+def get_compose_config(
+    containers, network, distro, container_fqdn, ips=IP_GENERATOR
+):
     """Create config for all containers in the list."""
 
     def node_dns_key(hostname):
@@ -23,6 +25,8 @@ def get_compose_config(containers, network, distro, ips=IP_GENERATOR):
     nodes = {}
     for container, ipaddr in zip(containers, ips):
         name = container["name"]
+        if container_fqdn:
+            name = ensure_fqdn(name, network.domain)
         node_distro = container.get("distro", distro)
         ip_address = f"{network.subnet}.{ipaddr}"
         hostname = get_hostname(container, name, network.domain)
@@ -61,6 +65,7 @@ def gen_compose_data(lab_config):
     Network = namedtuple("Network", ["domain", "networkname", "subnet", "dns"])
     labname = lab_config["lab_name"]
     subnet = lab_config["subnet"]
+    container_fqdn = lab_config["container_fqdn"]
     config = {"name": labname}
     networkname = f"ipanet-{labname}"
     config["networks"] = {
@@ -95,14 +100,18 @@ def gen_compose_data(lab_config):
         servers = cluster_config.get("servers")
         if servers:
             # First server must not have 'dns' set
-            ips, servers_cfg = get_compose_config([servers[0]], network, distro)
+            ips, servers_cfg = get_compose_config(
+                [servers[0]], network, distro, container_fqdn
+            )
             deployment_dns.append(next(iter(ips.values())))
             first_server_data = next(iter(servers_cfg.values()))
             first_server_data.pop("dns", None)
             services.update(servers_cfg)
             nodes.update(ips)
             # Replicas may have all settings
-            ips, servers_cfg = get_compose_config(servers[1:], network, distro)
+            ips, servers_cfg = get_compose_config(
+                servers[1:], network, distro, container_fqdn
+            )
             services.update(servers_cfg)
             nodes.update(ips)
         else:
@@ -110,7 +119,9 @@ def gen_compose_data(lab_config):
             deployment_dns.append(None)
         # Get clients configuration
         clients = cluster_config.get("clients")
-        ips, clients_cfg = get_compose_config(clients, network, distro)
+        ips, clients_cfg = get_compose_config(
+            clients, network, distro, container_fqdn
+        )
         services.update(clients_cfg)
         nodes.update(ips)
         # We must have at lest one node at the end.
